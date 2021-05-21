df242bb53cacf05e61dc36d21637a0262187368f
KPC-018A1819\K018A1819 <kai10u097@gmail.com>
2021-05-21 10:21:46 +0900

    タイトルのボタンのバグ修正

---
+++ 2021GameAward/Assets/Script/title_select.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class title_select : MonoBehaviour
+{
+    Button Button_start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button_start = GameObject.Find("Button_start").GetComponent<Button>();
+        Button_start.Select();
+    }
+
+    
+}
