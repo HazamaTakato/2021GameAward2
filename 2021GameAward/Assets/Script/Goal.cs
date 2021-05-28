505ab10dc2417376bad8ce8bbce31e0542986167
takatonzzz <hzmtakaton@gmail.com>
2021-05-28 14:17:45 +0900

    ゴールのプログラムを追加

---
+++ 2021GameAward/Assets/Script/Goal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    PlayerItem playerItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerItem = GetComponent<PlayerItem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerItem.GetItem)
+        {
+            transform.Rotate(new Vector3(0, 0, -5));
+        }
+    }
+}
